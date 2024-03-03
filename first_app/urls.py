ac1ddfd5bb432e67fd23ac14a0d2df239e61bdc8
Uglypr1nces <shpatavdiu4@gmail.com>
2024-03-03 14:09:40 +0100

    made a django project for my robot, made some functions and html webpages.

--- first_app/urls.py
+++ first_app/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('start/', views.start, name='start'),
     path('camera/', views.camera, name='camera'),
     path('sound/', views.sound, name='sound'),
-    path('shutdown/', views.shutdown, name='shutdown')
-
+    path('shutdown/', views.shutdown, name='shutdown'),
+    path('savesettings/',views.savesettings,name = "savesettings")
 ]
--- first_app/views.py
+++ first_app/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .python_robotcontroller.game import *
+from django.views.decorators.csrf import csrf_exempt
+from .python_robotcontroller.server.client import *
+import json
 import pygame
 import socket
 
-# Create your views here.
+port = 8001
+server = "0.0.0.0"
+
+def playsound(sound):
+    pygame.mixer.init()
+    pygame.mixer.music.load(f'{sound}.wav')
+    pygame.mixer.music.play()
 
 def index(request):
-    return render(request, 'base.html')
+    return render(request, 'home.html')
 def home(request):
     return render(request, 'home.html')
 def information(request):
@@ -16,19 +25,47 @@ def settings(request):
     return render(request, 'settings.html')
 
 def start(request):
-    print("starting robot..")
-    start_robot()
-    return HttpResponse("")
+    print(f"starting robot at {server}:{port}...")
+    if server == "0.0.0.0":
+        tkintermessage("please enter a server address in the settings page")
+    else:
+
+        client("start_sound",server,port)
+        start_robot(server,port)
+        return HttpResponse("")
 
 def camera(request):
-    print("opening camera..")
-    return HttpResponse("")
+    if server == "0.0.0.0":
+        tkintermessage("please enter a server address in the settings page")
+    else:
+        print("opening camera..")
+        return HttpResponse("")
 
 def sound(request):
-    print("playing sound..")
-    return HttpResponse("")
-
+    if server == "0.0.0.0":
+        tkintermessage("please enter a server address in the settings page")
+    else:
+        print("playing sound..")
+        client("alarm_sound", server, port)
+        return HttpResponse("")
 
 def shutdown(request):
-    print("shutting down..")
+    if server == "0.0.0.0":
+        tkintermessage("please enter a server address in the settings page")
+    else:
+        print("shutting down robot..")
+        client("over_sound",server,port)
+        return HttpResponse("")
+
+def savesettings(request):
+    global port
+    global server
+
+    if request.method == 'POST':
+        data = json.loads(request.body.decode('utf-8'))
+        server = data.get('robotIP')
+        port = int(data.get('port'))
+        print("Saving settings... Robot IP:", server, "Port:", port)
+        client("test",server,port)
+
     return HttpResponse("")
