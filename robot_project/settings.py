64eba00bcb784dc07d1f4fcdc975eecf7882be64
Uglypr1nces <shpatavdiu4@gmail.com>
2024-03-03 14:27:49 +0100

    changed project name

--- robot_project/settings.py
+++ robot_project/settings.py
@@ -14,6 +14,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 #configuration, static file paths, and more
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -120,8 +121,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
